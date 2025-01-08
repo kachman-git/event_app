1be40965c7d5e500431bf690610ec991f5a7c258
kachman-git <onyekachiokpala67@gmail.com>
2025-01-08 15:57:52 +0000

    Update sign up page.tsx

--- apps/web/app/signup/page.tsx
+++ apps/web/app/signup/page.tsx
@@ -62,7 +62,7 @@ export default function SignUp() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full max-w-md space-y-4 bg-white p-8 rounded-xl shadow-md">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full max-w-md space-y-4  p-8 rounded-xl shadow-md">
           <h2 className="text-2xl font-bold text-center mb-6">Sign Up</h2>
           <FormField
             control={form.control}
